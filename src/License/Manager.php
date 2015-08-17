bfa07e7a2ebcbc4fc466b1800043a12defd11aad
Barry Kooij <git@barrykooij.nl>
2015-08-17 16:34:46 +0200

    License Manager

---
+++ src/License/Manager.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Never5\LicenseWP\License;
+
+class Manager {
+
+	/**
+	 * Generate a new unique license key
+	 *
+	 * @return string
+	 */
+	public function generate_license_key() {
+		global $wpdb;
+
+		do {
+			// generate key
+			$key = apply_filters( 'license_wp_generate_licence_key', strtoupper( sprintf(
+				'%04x-%04x-%04x-%04x',
+				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ),
+				mt_rand( 0, 0x0fff ) | 0x4000,
+				mt_rand( 0, 0x3fff ) | 0x8000,
+				mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff ), mt_rand( 0, 0xffff )
+			) ) );
+
+			// check if exists
+			$result = $wpdb->get_row( $wpdb->prepare( 'SELECT FROM ' . $wpdb->prefix . 'license_wp_licenses WHERE license_key = %s', $key ) );
+
+		} while ( null !== $result ); // keep generating until we've got a unique key
+
+		// return key
+		return $key;
+	}
+
+}
