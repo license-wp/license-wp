0b72c63a442600993eda4e0715d464528c764d05
Barry Kooij <git@barrykooij.nl>
2015-08-17 16:36:08 +0200

    lost license form shortcode

---
+++ src/Shortcode/LostLicenseForm.php
@@ -0,0 +1,27 @@
+<?php
+namespace Never5\LicenseWP\Shortcode;
+
+class LostLicenseForm {
+
+	/**
+	 * __constructor
+	 */
+	public function __construct() {
+		add_shortcode( 'lost_licence_key_form', array( $this, 'view' ) );
+	}
+
+	/**
+	 * Shortcode view callback
+	 *
+	 * @return string
+	 */
+	public function view() {
+		ob_start();
+
+		// load template file via WooCommerce template function
+		wc_get_template( 'lost-licence-form.php', array(), 'license-wp', license_wp()->service( 'File' )->plugin_path() . '/templates/' );
+
+		return ob_get_clean();
+	}
+
+}
