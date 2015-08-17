9a11f6873412e7ccfff7b3707e4b87a45d07ce7c
Barry Kooij <git@barrykooij.nl>
2015-08-17 16:33:37 +0200

    assets class

---
+++ src/Assets.php
@@ -0,0 +1,33 @@
+<?php
+namespace Never5\LicenseWP;
+
+abstract class Assets {
+
+	/**
+	 * Enqueue frontend assets
+	 */
+	public static function enqueue_frontend() {
+	}
+
+	/**
+	 * Enqueue backend(admin) assets
+	 */
+	public static function enqueue_backend() {
+		global $pagenow, $post;
+
+		// only load WP media assets on correct page
+		if ( ( $pagenow == 'post.php' && isset( $post ) && PostType\ApiProduct::KEY === $post->post_type ) || ( $pagenow == 'post-new.php' && isset( $_GET['post_type'] ) && PostType\ApiProduct::KEY == $_GET['post_type'] ) ) {
+			wp_enqueue_media();
+		}
+
+		// admin CSS
+		wp_enqueue_style(
+			'license_wp_style',
+			license_wp()->service( 'file' )->plugin_url( '/assets/css/admin.css' ),
+			array(),
+			license_wp()->get_version()
+		);
+
+	}
+
+}
