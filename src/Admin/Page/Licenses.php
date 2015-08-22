ee8d07ac179057ecdbabb05e557a0115dc45cc7f
Barry Kooij <git@barrykooij.nl>
2015-08-22 11:45:20 +0200

    display licenses list table on licenses pae

--- src/Admin/Page/Licenses.php
+++ src/Admin/Page/Licenses.php
@@ -2,6 +2,8 @@
 
 namespace Never5\LicenseWP\Admin\Page;
 
+use \Never5\LicenseWP\License;
+
 /**
  * Class Licenses
  * @package Never5\LicenseWP\Admin\Pages
@@ -21,7 +23,25 @@ class Licenses extends Page {
 	 * @return void
 	 */
 	public function output() {
-		echo 'licenses';
+		
+		// create list table
+		$list_table = new License\ListTable(); 
+		
+		// prepare items in list table
+		$list_table->prepare_items();
+		?>
+		<div class="wrap">
+			<h2><?php _e( 'Licenses', 'license-wp' ); ?> <a
+					href="<?php echo admin_url( 'admin.php?page=license_wp_add_license' ); ?>"
+					class="add-new-h2"><?php _e( 'Add License', 'license-wp' ); ?></a></h2>
+
+			<form id="licence-management" method="post">
+				<input type="hidden" name="page" value="license_wp_licenses"/>
+				<?php $list_table->display() ?>
+				<?php wp_nonce_field( 'save', 'license_wp_licensing_nonce' ); ?>
+			</form>
+		</div>
+		<?php
 	}
 
 }
