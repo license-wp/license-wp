58bdccc71befd763d2e1d43a76e61ec71e38e325
Barry Kooij <git@barrykooij.nl>
2015-08-24 15:34:58 +0200

    use license manager to get license objects and pass these to email template

--- src/WooCommerce/Email.php
+++ src/WooCommerce/Email.php
@@ -26,13 +26,13 @@ class Email {
 		}
 
 		// fetch license keys
-		$licence_keys = $wpdb->get_results( $wpdb->prepare( "SELECT * FROM " . $wpdb->lwp_licenses . " WHERE order_id = %d", $order->id ) );
+		$licenses = license_wp()->service( 'license_manager' )->get_licenses_by_order( $order->id );
 
 		// check if we've found license keys
-		if ( null != $licence_keys ) {
+		if ( is_array( $licenses ) && count( $licenses ) > 0 ) {
 
 			// load our template file
-			wc_get_template( 'email-keys.php', array( 'keys' => $licence_keys ), 'license-wp', license_wp()->service( 'file' )->plugin_path() . '/templates/' );
+			wc_get_template( 'email-keys.php', array( 'licenses' => $licenses ), 'license-wp', license_wp()->service( 'file' )->plugin_path() . '/templates/' );
 		}
 	}
 
