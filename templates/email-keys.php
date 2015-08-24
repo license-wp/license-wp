81fdd14927bf3489d11b6a47c43a3afc616069f4
Barry Kooij <git@barrykooij.nl>
2015-08-24 15:35:35 +0200

    License keys email template file

---
+++ templates/email-keys.php
@@ -0,0 +1,26 @@
+<?php if ( count( $licenses ) > 0 ) : ?>
+
+	<h2><?php _e( 'License Keys', 'license-wp' ); ?></h2>
+	<ul>
+		<?php
+		foreach ( $licenses as $license ) :
+			$wc_product = \Never5\LicenseWP\WooCommerce\Product::get_product( $license->get_product_id() );
+			?>
+			<li>
+				<?php echo esc_html( $wc_product->post_title ); ?>: <strong><?php echo $license->get_key(); ?></strong>
+				<?php
+				// license products
+				$api_products = $license->get_api_products();
+				if ( count( $api_products ) > 0 ) {
+					echo '<ul class="digital-downloads">';
+					foreach ( $api_products as $api_product ) {
+						echo '<li><a class="download-button" href="' . $api_product->download_url( $license ) . '">' . sprintf( __( 'Download %s', 'license-wp' ), $api_product->get_name() ) . '</a></li>';
+					}
+					echo '</ul>';
+				}
+				?>
+			</li>
+		<?php endforeach; ?>
+	</ul>
+
+<?php endif; ?>
