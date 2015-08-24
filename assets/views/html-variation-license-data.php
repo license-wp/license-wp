837f5da34cabf1bda76822c852a9fb21a786f946
Barry Kooij <git@barrykooij.nl>
2015-08-24 15:22:23 +0200

    typo fixes in view

--- assets/views/html-variation-license-data.php
+++ assets/views/html-variation-license-data.php
@@ -3,15 +3,15 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 ?>
-<tr class="show_if_is_api_product_licence">
+<tr class="show_if_is_api_product_license">
 	<td>
 		<div>
 			<?php woocommerce_wp_text_input( array(
-				'id'                => '_variation_licence_activation_limit_' . $loop,
-				'name'              => '_variation_licence_activation_limit[' . $loop . ']',
+				'id'                => '_variation_license_activation_limit_' . $loop,
+				'name'              => '_variation_license_activation_limit[' . $loop . ']',
 				'label'             => __( 'Licence activation limit', 'wp-plugin-licencing' ) . ':',
 				'placeholder'       => __( 'Inherit from parent', 'wp-plugin-licencing' ),
-				'value'             => get_post_meta( $variation->ID, '_licence_activation_limit', true ),
+				'value'             => get_post_meta( $variation->ID, '_license_activation_limit', true ),
 				'type'              => 'number',
 				'class'             => '',
 				'custom_attributes' => array(
@@ -24,11 +24,11 @@ if ( ! defined( 'ABSPATH' ) ) {
 	<td>
 		<div>
 			<?php woocommerce_wp_text_input( array(
-				'id'                => '_variation_licence_expiry_days_' . $loop,
-				'name'              => '_variation_licence_expiry_days[' . $loop . ']',
+				'id'                => '_variation_license_expiry_days_' . $loop,
+				'name'              => '_variation_license_expiry_days[' . $loop . ']',
 				'label'             => __( 'Licence expiry days', 'wp-plugin-licencing' ) . ':',
 				'placeholder'       => __( 'Inherit from parent', 'wp-plugin-licencing' ),
-				'value'             => get_post_meta( $variation->ID, '_licence_expiry_days', true ),
+				'value'             => get_post_meta( $variation->ID, '_license_expiry_days', true ),
 				'type'              => 'number',
 				'class'             => '',
 				'custom_attributes' => array(
