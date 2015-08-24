1e044d689231a5a0e59022412531888aa4cc87cc
Barry Kooij <git@barrykooij.nl>
2015-08-24 15:58:39 +0200

    WooCommerce MyAccount class

---
+++ src/WooCommerce/MyAccount.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace Never5\LicenseWP\WooCommerce;
+
+/**
+ * Class MyAccount
+ * @package Never5\LicenseWP\WooCommerce
+ */
+class MyAccount {
+
+	/**
+	 * Setup hooks
+	 */
+	public function setup() {
+		add_action( 'woocommerce_before_my_account', array( $this, 'print_licenses' ) );
+	}
+
+	/**
+	 * Print licenses
+	 */
+	public function print_licenses() {
+
+		// get licenses of current user
+		$licenses = license_wp()->service( 'license_manger' )->get_licenses_by_user( get_current_user_id() );
+
+		if ( count( $licenses ) > 0 ) {
+			wc_get_template( 'my-licenses.php', array( 'licenses' => $licenses ), 'license-wp', license_wp()->service( 'file' )->plugin_path() . '/templates/' );
+		}
+
+	}
+
+}
