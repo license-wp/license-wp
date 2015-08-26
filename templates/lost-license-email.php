f37de4abf1e7b4271fdd9fc57ecd443dc54d74b2
Barry Kooij <git@barrykooij.nl>
2015-08-26 16:09:12 +0200

    lost license email template file

---
+++ templates/lost-license-email.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Lost license email
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+} // Exit if accessed directly
+
+if ( $user_first_name ) {
+	echo sprintf( __( "Hello %s,", 'license-wp' ), $user_first_name ) . "\n\n";
+} else {
+	echo __( "Hi there,", 'license-wp' ) . "\n\n";
+}
+_e( "Your license keys and product download links are listed below.", 'license-wp' );
+echo "\n";
+
+// loop through licenses
+foreach ( $licenses as $license ) {
+	// license products
+	$api_products = $license->get_api_products();
+
+	// check & loop
+	if ( isset( $api_products ) && count( $api_products ) > 0 ) {
+		foreach ( $api_products as $api_product ) {
+			echo "\n====================\n";
+			echo esc_html( get_the_title( $api_product->get_id() ) ) . ': ' . $api_product->download_url( $license ) . "\n";
+			echo $license->get_key() . "";
+			echo "\n====================\n\n";
+		}
+	}
+}
+
+_e( "You can manage your licenses and download your products from your My Account page.", 'license-wp' );
+echo "\n";
+echo "\n";
+
+// Footer
+echo '--' . "\n";
+echo apply_filters( 'woocommerce_email_footer_text', get_option( 'woocommerce_email_footer_text' ) );
