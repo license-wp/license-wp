8bf68aa8f8d48a4baac366efdd96f516e6671641
Barry Kooij <git@barrykooij.nl>
2015-09-01 08:46:38 +0200

    updated composer files

--- vendor/composer/autoload_namespaces.php
+++ vendor/composer/autoload_namespaces.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(dirname(__FILE__));
 $baseDir = dirname($vendorDir);
 
 return array(
+    'Parsedown' => array($vendorDir . '/erusev/parsedown'),
     '' => array($vendorDir . '/wpupdatephp/wp-update-php/src'),
 );
